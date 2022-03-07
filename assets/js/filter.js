baea9b4e852fc59c0d87cc82bc3099f73f54afe3
esraayounis <esraayounis61@gmail.com>
2022-03-07 09:02:00 +0200

    details pages

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     
     $(".filter-button").click(function(){
+        debugger
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -17,4 +18,22 @@ $(document).ready(function(){
         }
     }
   });
+
+
+//   $(".filter-exam").click(function(){
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById("myInputExam");
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("examinations");
+//     li = ul.getElementsByClassName("exam");
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByClassName("w-100 mx-4")[0];
+//         txtValue = a.textContent || a.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+//   });
 });
