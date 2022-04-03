acd79f138f49a4d94cc955955945628b8e329bd9
esraayounis <esraayounis61@gmail.com>
2022-04-03 15:36:17 +0200

    fix reservations

--- assets/js/get-reservation-data.js
+++ assets/js/get-reservation-data.js
@@ -4,6 +4,8 @@ window.onload = function () {
     document.getElementById('succuss-discount-text').style.display ="none";
     document.getElementById('delete-code-btn').style.display ="none";
     document.getElementById('div-discount').style.display ="none";
+    document.getElementById('select-payment-method').style.display ="none";
+    document.getElementById('visa-data').style.display ="none";
 
     setTimeout(() => {
     document.querySelector('.value-card').style.display ="none";
@@ -66,18 +68,29 @@ window.onload = function () {
 
 //  Payment Method
     $('input[type="radio"]').click(function() {
-        debugger
+
+        document.getElementById('select-payment-method').style.display ="none";
         var inputValue = $(this).attr("value");
         var targetBox = $("." + inputValue);
         $(".box").not(targetBox).hide();
         $(targetBox).show();
-        if(paymentMethod == "visa" || paymentMethod == "epay"){
+        var paymentMethod;
+        if(inputValue == "visa")
+        {
+            localStorage.setItem("visa-payment", inputValue);
+        }
+        else{
+            localStorage.setItem("visa-payment", "");
+        }
+        if(inputValue == "visa" || inputValue == "epay"){
             paymentMethod = "تم الدفع"
         }
-        else if(paymentMethod == "cash"){
+        else if(inputValue == "cash"){
             paymentMethod = "الدفع عند الاستلام"
         }
-        localStorage.setItem("payment-method", paymentMethod)
+        localStorage.setItem("payment-method", paymentMethod);
+
+
     });
 
 
@@ -123,97 +136,179 @@ window.onload = function () {
         document.getElementById('error-discount-text').style.display ="none";
     }
 
-    document.querySelector('.cc-number').oninput = () =>{
-        var ccNumber = document.querySelector('.cc-number').value;
-        document.querySelector('.cc-number').innerText = ccNumber;
-    }
-
-    // document.querySelector('.cc-number-mob').oninput = () =>{
-    //     var ccNumber = document.querySelector('.cc-number-mob').value;
-    //     document.querySelector('.cc-number-mob').innerText = ccNumber;
-    // }
-
-    document.querySelector('#exp-month').oninput = () =>{
-        var expMonth = document.querySelector('#exp-month').value;
-        document.querySelector('#exp-month').innerText = expMonth;
-    }
-
-    document.querySelector('#exp-year').oninput = () =>{
-        var expYear = document.querySelector('#exp-year').value;
-        document.querySelector('#exp-year').innerText = expYear;
-    }
-
-    document.querySelector('.cc-cvc').oninput = () =>{
-        var cvv = document.querySelector('.cc-cvc').value;
-        document.querySelector('.cc-cvc').innerText = cvv;
-    }
 
     function checkRadio()
     {
-        var payment = document.querySelector('.pay');
+        debugger
+        var payment = document.querySelectorAll('.payment');
         var paymentType = '';
-        for(i = 0; i < payment.length; i++)
+        for(var i in  payment){
+            if(payment[i].checked == true)
             {
-            console.log(payment[i].type + ":" + payment[i].value);
-                if(payment[i].checked)
-                {
-                    console.log(payment[i].value)
-                    paymentType = payment[i].value;
-                }
-        }
+              paymentType = payment[i].value;
+            }
+           
+        } 
         return paymentType != '' ? true : false;
     }
 
 
     function buyNow(){
-        debugger
+        
         var payment = checkRadio();
         if (payment == false)
         {
-            document.getElementById('payment-type').style.display ="block";
+            document.getElementById('select-payment-method').style.display ="block";
+            return false;
         }
         else{
-            document.getElementById('payment-type').style.display ="none";
+            document.getElementById('select-payment-method').style.display ="none";
         }
 
+    
+       
+        var image = document.querySelector('.avatar').innerHTML;
+
+        var title = document.querySelector('.title-card').innerHTML;
+
+        var address = document.querySelector('.address').innerHTML;
+
         var reservationDate = document.getElementById('date').innerHTML;
-        localStorage.setItem("reservation-date", reservationDate);
 
         var reservationDiscount = document.getElementById('discount-value').innerHTML;
-        localStorage.setItem("reservation-discount", reservationDiscount);
 
         if(reservationDiscount != ""){
             var reservationTotalPrice = document.getElementById('total').innerHTML;
-            localStorage.setItem("reservation-total-price", reservationTotalPrice);
         }
         else{
             var reservationTotalPrice = document.getElementById('totalPrice').innerHTML;
-            localStorage.setItem("reservation-total-price", reservationTotalPrice);
+            reservationDiscount = "لا يوجد"
         }
 
-        var paymentMethod =  localStorage.getItem("payment-method")
-       
+        var paymentMethod =  localStorage.getItem("payment-method");
+         var reservationList =  document.querySelector('.reservationList');
+         const elemObj = `<div class="card-box p-2 py-4 card-detail">
+            <div class="row">
+             <div class="col-lg-3 p-lg-0">
+                 <div class="avatar text-center">${image}</div>
+             </div>
+             <div class="col-lg-6 p-lg-0 detail">
+                 <h6 class="title-card txt-md"> ${title}</h6>
+              
+                 <div class="d-flex align-items-center">
+                  <p><span class="reserve-title" >العنوان:</span></p>
+                  <p >${address}</p>
+                 </div>
+                
+                <div class="align-items-center check-branch">
+                    <p><span class="reserve-title">الفرع:</span></p>
+                    <p >${branch}</p>
+                </div>
+
+                <div class="d-flex align-items-center">
+                    <p><span class="reserve-title">التاريخ:</span></p>
+                    <p >${reservationDate}</p>
+                </div>
+
+                <div class="d-flex align-items-center">
+                  <p><span class="reserve-title">الخصم:</span></p>
+                  <p>${reservationDiscount}</p>
+                </div>
+
+                <div class="d-flex align-items-center">
+                    <p><span class="reserve-title">الأجمالي:</span></p>
+                    <p >${reservationTotalPrice}</p>
+                </div>
+             </div>
+             <div class="col-lg-3">
+             <div class="txt-payment paid">
+             </div>
+             
+              <span class="txt-payment not-paid"></span>
+             </div>
+            </div>
+         </div>`
+         reservationList.insertAdjacentHTML('beforeend',elemObj);
+         console.log(reservationList)
+debugger
+          
+        if(paymentMethod == "الدفع عند الاستلام"){
+            var divUnPaid =  document.querySelector('.paid');
+            var unpaid =  `${paymentMethod}`
+            divUnPaid.insertAdjacentHTML('beforeend',unpaid);
+        }
+
+        else{
+    
+            var divPaid =  document.querySelector('.paid');
+            var paid =  `<label class="">
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+                ${paymentMethod}
+                </label>`
+            divPaid.insertAdjacentHTML('beforeend',paid);
+             
+        }
+        
+
+        var serviceNum =  localStorage.getItem("service-num");
+        var serviceName = localStorage.getItem("selected-service");
+        
+        if (serviceNum == "3" || serviceName == " زيارة طبيب "){
+          document.querySelector('.check-branch').style.display = "none";
+          document.querySelector('.stars-special').style.display = "none";
+          
+        }
+        else{
+            document.querySelector('.check-branch').style.display = "flex";
+            var branch = document.querySelector('.selected-address .current').innerHTML;
+        }
+
+         localStorage.setItem('reservationPage',reservationList.innerHTML);
+
+        document.querySelector('.cc-number').oninput = () =>{
+            var ccNumber = document.querySelector('.cc-number').value;
+            document.querySelector('.cc-number').innerText = ccNumber;
+        }
+    
         var media_query = 'screen and (max-width:600px)';
         var matched = window.matchMedia(media_query).matches;
         if(matched){
-            var card =  localStorage.getItem("card-mob");
+            document.querySelector('.cc-number-mob').oninput = () =>{
+                var ccNumber = document.querySelector('.cc-number-mob').value;
+                document.querySelector('.cc-number-mob').innerText = ccNumber;
+            }
         }
         else{
-            var card =  localStorage.getItem("card-web");
+            document.querySelector('#exp-month').oninput = () =>{
+                var expMonth = document.querySelector('#exp-month').value;
+                document.querySelector('#exp-month').innerText = expMonth;
+            }
         }
-        
-        
-         var reservationObjects = {
-            card:card,
-            date:reservationDate,
-            price:reservationTotalPrice,
-            discount:reservationDiscount,
-            paymentmethod:paymentMethod
-         }
+    
+        document.
+        querySelector('#exp-year').oninput = () =>{
+            var expYear = document.querySelector('#exp-year').value;
+            document.querySelector('#exp-year').innerText = expYear;
+        }
+    
+        document.querySelector('.cc-cvc').oninput = () =>{
+            var cvv = document.querySelector('.cc-cvc').value;
+            document.querySelector('.cc-cvc').innerText = cvv;
+        }
+      
+        var visa = localStorage.getItem("visa-payment")
+        if(visa == "visa" && document.querySelector('.cc-cvc').value == ''){
 
-         var reservationList =  [];
-         reservationList.push(reservationObjects); 
-         console.log(reservationList)
-         localStorage.setItem('reservationPage', JSON.stringify(reservationList));
+            document.getElementById('visa-data').style.display ="block";
+            return false;
+        }
+
+        else{
+            document.getElementById('select-payment-method').style.display ="none";
+            document.getElementById('visa-data').style.display ="none";
+        }
+
+
         window.location.href ="success.html";
     }
